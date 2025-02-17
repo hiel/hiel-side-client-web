e9e360dde6a49c961fda378ee093f4502098b575
hiel <yangjunghooon@gmail.com>
2025-02-17 16:54:50 +0900

    Fix: Update cicd

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: process.env.NEXT_PUBLIC_USE_STRICT_MODE === true && process.env.NEXT_PUBLIC_ENVIRONMENT === "local",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_S3_BUCKET_URL,
-      },
-    ],
-  },
   eslint: {
     ignoreDuringBuilds: false,
   },
